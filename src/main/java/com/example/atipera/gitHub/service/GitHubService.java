3b93d729bd8e511b0a7cffdd937739b3b71937a2
Luzu4 <maciekluz@gmail.com>
2023-06-06 13:02:06 +0200

    gitHubService refactored

--- src/main/java/com/example/atipera/gitHub/service/GitHubService.java
+++ src/main/java/com/example/atipera/gitHub/service/GitHubService.java
@@ -9,6 +9,9 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GitHubService {
 
+    private static final String BRANCHES_URL = "https://api.github.com/repos/{OWNER}/{REPO}/branches";
+    private static final String REPOSITORIES_URL = "https://api.github.com/users/{USERNAME}/repos";
+
     private final RestTemplate restTemplate;
 
     public GitHubService(RestTemplateBuilder restTemplateBuilder) {
@@ -16,10 +19,10 @@ public class GitHubService {
     }
 
     public GitHubRepositoryDTO[] getAllRepositoriesByUsername(String username) {
-        return restTemplate.getForEntity("https://api.github.com/users/" + username + "/repos", GitHubRepositoryDTO[].class).getBody();
+        return restTemplate.getForEntity(REPOSITORIES_URL, GitHubRepositoryDTO[].class, username).getBody();
     }
 
     public GitHubBranchDTO[] getAllBranchesByRepositoryNameAndUsername(String userName, String repositoryName) {
-        return restTemplate.getForEntity("https://api.github.com/repos/" + userName + "/" + repositoryName + "/branches", GitHubBranchDTO[].class).getBody();
+        return restTemplate.getForEntity(BRANCHES_URL, GitHubBranchDTO[].class, userName,repositoryName).getBody();
     }
 }
